386ded098f781e04bfa5e45c4906151685291ee1
= <=>
2020-06-19 19:27:32 +0800

    first

---
+++ index.php
@@ -0,0 +1,428 @@
+<!DOCTYPE html>
+<html >
+<head>
+	<title></title>
+	<meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+	<meta content="utf-8" http-equiv="encoding">
+	<link rel="stylesheet" type="text/css" href="/shop/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="/shop/css/custom.css">
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<!-- HEADER AREA -->
+<nav class="navbar navbar-expand-md bg-orange navbar-dark">
+    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+        <div class="navbar-nav left-nav">
+        		<a href="#" class="nav-item nav-link active">Home</a>
+	            <a href="#" class="nav-item nav-link">Profile</a>
+	            <a href="#" class="nav-item nav-link">Profile</a>
+	            <a href="#" class="nav-item nav-link">Follow us on</a>
+	            <a class="fa fa-facebook nav-link mt-1 social-icon" title="" target="_blank" href="#"></a>
+	            <a class="fa fa-instagram nav-link mt-1 social-icon" title="" target="_blank" href="#"></a>
+        </div>
+        <div class="navbar-nav">
+        	<a href="#" class="nav-item nav-link font-weight-bold">Signup</a>
+        	<span class="nav-divider text-white">|</span>
+            <a href="#" class="nav-item nav-link font-weight-bold">Login</a>
+        </div>
+    </div>
+</nav>
+<div class="container-fluid bg-lightorange">
+<section class="px-4 pt-5 pb-4">
+	<div class="row">
+		<div class="col-md-2">
+			<a href="#" class="navbar-brand">
+				<img src="/shop/images/logo/logo.png" alt="Logo">
+			</a>
+		</div>	
+		<div class="col-md-8">
+			 <form class="form-inline">
+			    <div class="input-group w-100">                    
+			        <input type="text" class="form-control" placeholder="Search">
+			        <div class="input-group-append bg-orange">
+			            <button type="button" class="btn btn-secondary bg-orange mx-2 w-100"><i class="fa fa-search"></i></button>
+			        </div>
+			    </div>
+			</form>
+			<div class="search-links">
+				<a href="">Home</a>
+				<a href="">About</a>
+				<a href="">About</a>
+				<a href="">About</a>
+				<a href="">About</a>
+				<a href="">About</a>
+				<a href="">About</a>
+			</div>
+		</div>	
+		<div class="col-md-2">
+			<a href="#" class="cart-icon text-white"><i class="fa fa-shopping-cart" style="font-size:36px"></i></a>
+		</div>		
+	</div>
+</section>
+</div>
+<!-- END OF HEADER AREA -->
+
+<!-- BANNER AREA -->
+<div class="container-fluid">
+<section class="px-4 pt-5 pb-4">
+	<div class="row">
+		<div class="col-md-8">
+			<div id="bannerCarousel" class="carousel slide" data-ride="carousel">
+			    <!-- Carousel indicators -->
+			    <ol class="carousel-indicators">
+			        <li data-target="#bannerCarousel" data-slide-to="0" class="active"></li>
+			        <li data-target="#bannerCarousel" data-slide-to="1"></li>
+			        <li data-target="#bannerCarousel" data-slide-to="2"></li>
+			    </ol>
+			    <!-- Wrapper for carousel items -->
+			    <div class="carousel-inner">
+			        <div class="carousel-item active">
+			            
+			        </div>
+			        <div class="carousel-item">
+			            
+			        </div>
+			        <div class="carousel-item">
+			         
+			        </div>
+			    </div>
+			    <!-- Carousel controls -->
+			    <a class="carousel-control-prev" href="#bannerCarousel" data-slide="prev">
+			        <span class="carousel-control-prev-icon"></span>
+			    </a>
+			    <a class="carousel-control-next" href="#bannerCarousel" data-slide="next">
+			        <span class="carousel-control-next-icon"></span>
+			    </a>
+			</div>
+		</div>	
+		<div class="col-md-4 side-banner">
+			 <div class="col-md-12 bg-danger text-white">
+			 	<p class="text-center">BANNER IMAGE/TEXT HERE</p>
+			 </div>
+			 <div class="col-md-12 bg-success text-white mx-auto">
+			 	<p class="text-center">BANNER IMAGE/TEXT HERE</p>
+			 </div>
+		</div>		
+	</div>
+</section>
+<!-- END OF BANNER AREA -->
+
+<!-- PRODUCTS -->
+<section>
+	<div class="row">
+		<div class="col-md-12">
+			<h4 class="mb-4 d-block">FLASH <b>DEALS</b></h2>
+			<div id="myCarousel" class="carousel slide" data-ride="carousel" data-interval="0">
+			<!-- Carousel indicators -->
+			<ol class="carousel-indicators">
+				<li data-target="#myCarousel" data-slide-to="0" class=""></li>
+				<li data-target="#myCarousel" data-slide-to="1" class=""></li>
+				<li data-target="#myCarousel" data-slide-to="2" class="active"></li>
+			</ol>   
+			<!-- Wrapper for carousel items -->
+			<div class="carousel-inner">
+				<div class="item carousel-item">
+					<div class="row">
+						<div class="col-sm-3">
+							<div class="thumb-wrapper">
+								<div class="img-box">
+									<img src="https://www.tutorialrepublic.com/examples/images/products/iphone.jpg" class="img-responsive img-fluid" alt="">
+								</div>
+								<div class="thumb-content">
+									<h4><strike>$400.00</strike> <span>$369.00</span></h4>
+									<div class="star-rating">
+										<ul class="list-inline">
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+										</ul>
+									</div>
+								</div>						
+							</div>
+						</div>
+						<div class="col-sm-3">
+							<div class="thumb-wrapper">
+								<div class="img-box">
+									<img src="https://www.tutorialrepublic.com/examples/images/products/headphone.jpg" class="img-responsive img-fluid" alt="">
+								</div>
+								<div class="thumb-content">
+									<h4>$400.00</h4>
+									<div class="star-rating">
+										<ul class="list-inline">
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+										</ul>
+									</div>
+								</div>						
+							</div>
+						</div>		
+						<div class="col-sm-3">
+							<div class="thumb-wrapper">
+								<div class="img-box">
+									<img src="https://www.tutorialrepublic.com/examples/images/products/macbook-air.jpg" class="img-responsive img-fluid" alt="">
+								</div>
+								<div class="thumb-content">
+									<h4>$400.00</h4>
+									<div class="star-rating">
+										<ul class="list-inline">
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star-half-o"></i></li>
+										</ul>
+									</div>
+								</div>						
+							</div>
+						</div>								
+						<div class="col-sm-3">
+							<div class="thumb-wrapper">
+								<div class="img-box">
+									<img src="https://www.tutorialrepublic.com/examples/images/products/nikon.jpg" class="img-responsive img-fluid" alt="">
+								</div>
+								<div class="thumb-content">
+									<h4>$400.00</h4>
+									<div class="star-rating">
+										<ul class="list-inline">
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+										</ul>
+									</div>
+								</div>						
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="item carousel-item active">
+					<div class="row">
+						<div class="col-sm-3">
+							<div class="thumb-wrapper">
+								<div class="img-box">
+									<img src="https://www.tutorialrepublic.com/examples/images/products/iphone.jpg" class="img-responsive img-fluid" alt="">
+								</div>
+								<div class="thumb-content">
+									<h4>$369.00</h4>
+									<div class="star-rating">
+										<ul class="list-inline">
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+										</ul>
+									</div>
+								</div>						
+							</div>
+						</div>
+						<div class="col-sm-3">
+							<div class="thumb-wrapper">
+								<div class="img-box">
+									<img src="https://www.tutorialrepublic.com/examples/images/products/canon.jpg" class="img-responsive img-fluid" alt="">
+								</div>
+								<div class="thumb-content">
+									<h4>$369.00</h4>
+									<div class="star-rating">
+										<ul class="list-inline">
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+										</ul>
+									</div>
+								</div>						
+							</div>
+						</div>
+						<div class="col-sm-3">
+							<div class="thumb-wrapper">
+								<div class="img-box">
+									<img src="https://www.tutorialrepublic.com/examples/images/products/pixel.jpg" class="img-responsive img-fluid" alt="">
+								</div>
+								<div class="thumb-content">
+									<h4>$400.00</h4>
+									<div class="star-rating">
+										<ul class="list-inline">
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+										</ul>
+									</div>
+								</div>						
+							</div>
+						</div>	
+						<div class="col-sm-3">
+							<div class="thumb-wrapper">
+								<div class="img-box">
+									<img src="https://www.tutorialrepublic.com/examples/images/products/watch.jpg" class="img-responsive img-fluid" alt="">
+								</div>
+								<div class="thumb-content">
+									<h4>$400.00</h4>
+									<div class="star-rating">
+										<ul class="list-inline">
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star"></i></li>
+											<li class="list-inline-item"><i class="fa fa-star-o"></i></li>
+										</ul>
+									</div>
+								</div>						
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Carousel controls -->
+			<a class="carousel-control left carousel-control-prev" href="#myCarousel" data-slide="prev">
+				<i class="fa fa-angle-left"></i>
+			</a>
+			<a class="carousel-control right carousel-control-next" href="#myCarousel" data-slide="next">
+				<i class="fa fa-angle-right"></i>
+			</a>
+		</div>
+		</div>
+	</div>
+</section>
+
+<section>
+	<h4 class="mb-4 d-block">PRODUCTS</b></h2>
+	 <div class="row">
+        <div class="col-md-2 col-sm-6">
+            <div class="product-grid2">
+                <div class="product-image2"> <a href="#"> <img class="pic-1" src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1561643954/img-1.jpg"> <img class="pic-2" src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1561643955/img1.2.jpg"> </a>
+                    <ul class="social">
+                        <li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
+                        <li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
+                    </ul>
+                </div>
+                <div class="product-content">
+                    <h3 class="title"><a href="#">Women's Top</a></h3> <p class="item-price text-orange"><strike>$400.00</strike> <span>$369.00</span></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-2 col-sm-6">
+            <div class="product-grid2">
+                <div class="product-image2"> <a href="#"> <img class="pic-1" src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1561643957/img-2.jpg"> <img class="pic-2" src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1561643954/img-2.2.jpg"> </a>
+                    <ul class="social">
+                        <li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
+                        <li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
+                    </ul>
+                </div>
+                <div class="product-content">
+                    <h3 class="title"><a href="#">Women's Top</a></h3> <p class="item-price text-orange"><strike>$400.00</strike> <span>$369.00</span></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-2 col-sm-6">
+            <div class="product-grid2">
+                <div class="product-image2"> <a href="#"> <img class="pic-1" src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1561643954/img-3.jpg"> <img class="pic-2" src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1561643960/img-1.3.jpg"> </a>
+                    <ul class="social">
+                        <li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
+                        <li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
+                    </ul>
+                </div>
+                <div class="product-content">
+                    <h3 class="title"><a href="#">Women's Top</a></h3> <p class="item-price text-orange"><strike>$400.00</strike> <span>$369.00</span></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-2 col-sm-6">
+            <div class="product-grid2">
+                <div class="product-image2"> <a href="#"> <img class="pic-1" src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1561643954/img-3.jpg"> <img class="pic-2" src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1561643960/img-1.3.jpg"> </a>
+                    <ul class="social">
+                        <li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
+                        <li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
+                    </ul>
+                </div>
+                <div class="product-content">
+                    <h3 class="title"><a href="#">Women's Top</a></h3> <p class="item-price text-orange"><strike>$400.00</strike> <span>$369.00</span></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-2 col-sm-6">
+            <div class="product-grid2">
+                <div class="product-image2"> <a href="#"> <img class="pic-1" src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1561643954/img-3.jpg"> <img class="pic-2" src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1561643960/img-1.3.jpg"> </a>
+                    <ul class="social">
+                        <li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
+                        <li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
+                    </ul>
+                </div>
+                <div class="product-content">
+                    <h3 class="title"><a href="#">Women's Top</a></h3> <p class="item-price text-orange"><strike>$400.00</strike> <span>$369.00</span></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-2 col-sm-6">
+            <div class="product-grid2">
+                <div class="product-image2"> <a href="#"> <img class="pic-1" src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1561643954/img-3.jpg"> <img class="pic-2" src="https://res.cloudinary.com/dxfq3iotg/image/upload/v1561643960/img-1.3.jpg"> </a>
+                    <ul class="social">
+                        <li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
+                        <li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
+                    </ul>
+                </div>
+                <div class="product-content">
+                    <h3 class="title"><a href="#">Women's Top</a></h3> <p class="item-price text-orange"><strike>$400.00</strike> <span>$369.00</span></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- END OF PRODUCTS -->
+</div>
+<!-- FOOTER AREA -->
+<footer>
+	<section class="bg-lightorange text-white">
+		<div class="container-fluid">
+			<div class="row py-4 px-4">
+				<div class="col-md-3">
+					<b>Address</b>
+					<p>1234 Street Name </p>
+					<p>City, AA 99999 </p>
+
+					<b class="mt-3 d-block">Contacts</b>
+					<p>Email: support@mobirise.com</p>
+					<p>Phone: +1 (0) 000 0000 001</p>
+					<p>Fax: +1 (0) 000 0000 002 </p>
+				</div>
+				<div class="col-md-3">
+					<b>Links</b><br>
+					<a href="">Home</a><br>
+					<a href="">About</a><br>
+					<a href="">FAQ</a><br>
+
+					<b class="mt-3 d-block">Feedback</b>
+					<p>Please send us your ideas, bug reports, suggestions! Any feedback would be appreciated.</p>
+				</div>
+				<div class="col-md-6">
+					<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d31402.04877128853!2d123.85564406992478!3d10.321378228393456!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33a99ecedffd4d93%3A0x6c00a0cf0efb2913!2sGuadalupe%2C%20Cebu%20City%2C%20Cebu!5e0!3m2!1sen!2sph!4v1592561570948!5m2!1sen!2sph" width="100%" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+				</div>
+			</div>
+		</div>
+		<div class="row bg-orange">
+			<div class="col-md-12 py-2 px-2">
+				<p class="text-center">© Copyright 2019</p>
+			</div>
+		</div>
+	</section>
+</footer>
+
+
+<script type="text/javascript" src="/shop/js/jquery-3.4.1.min.js"></script>
+<script type="text/javascript" src="/shop/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+</body>
+</html>
